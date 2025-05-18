ca637b68e92cd71023ee5e05d3359010966425ec
ADMIN <jpbasavaraj118@gmail.com>
2025-05-18 11:26:00 +0530

    workhistory section added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { WorkhistoryComponent } from './workhistory/workhistory.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent },  // Default route
   { path: 'about', component: AboutComponent },  // Navigates to About page
   { path: 'skills', component: SkillsComponent }, // Corrected route
+  {path:'workhistory',component: WorkhistoryComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown paths
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { SkillsComponent } from './skills/skills.component';
+import { WorkhistoryComponent } from './workhistory/workhistory.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SkillsComponent } from './skills/skills.component';
     NavbarComponent,
     AboutComponent,
     FooterComponent,
-    SkillsComponent
+    SkillsComponent,
+    WorkhistoryComponent
   ],
   imports: [
     BrowserModule,
